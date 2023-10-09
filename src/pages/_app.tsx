d481040141f4c7ac352027c8d8c339567cfeb8c7
Ricardo Henrique Vinciguerra <ricardorhv.dev@gmail.com>
2023-10-09 19:18:44 -0300

    feat: setting the routes and style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+/* eslint-disable react/no-unknown-property */
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
